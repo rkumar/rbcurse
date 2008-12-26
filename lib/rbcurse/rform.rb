ea4eef282aed02ae369c43f8270f56216dec249c
Rahul Kumar <rahulbenegal@gmail.com>
2008-12-27 01:08:02 +0530

    bug fixes in textarea cursor movement
    with respect to wrapping lines
    backspace behaviour.
    newlines
    still some glitches.

--- lib/rbcurse/rform.rb
+++ lib/rbcurse/rform.rb
@@ -4,7 +4,7 @@
   * Description   Our own form with own simple field to make life easier. Ncurses forms are great, but
   *         honestly the sequence sucks and is a pain after a while for larger scale work.
   *         We need something less restrictive.
-  * Author: rkumar
+  * Author: rkumar (arunachalesha)
 TODO 
   * Field/entry
     - textvariable - bding field to a var so the var is updated
@@ -155,7 +155,7 @@ module RubyCurses
         addcol(@buffer.length-@curpos)+1
         @curpos = @buffer.length
       end
-      $log.debug "TA: after : curpos #{@curpos} blen: #{@buffer.length}"
+      $log.debug "TA: after : curpos #{@curpos} blen: #{@buffer.length}, w: #{@width} max #{@maxlen}"
       pre_key
       case ch
       when ?\C-n
@@ -184,6 +184,7 @@ module RubyCurses
       when KEY_ENTER, 10, 13
         return -1 unless @editable
         # insert a blank row and append rest of this line to cursor
+        $log.debug "ENTER PRESSED at  #{@curpos}, on row #{@prow}"
         @delete_buffer = (delete_eol || "")
         @list[@prow] << "\r"
         $log.debug "DELETE BUFFER #{@delete_buffer}" 
@@ -228,7 +229,8 @@ module RubyCurses
       when ?\C-a
         set_form_col 0
       when ?\C-e
-        set_form_col @buffer.length
+        cursor_eol
+        #set_form_col @buffer.length
       else
         #$log.debug(" textarea ch #{ch}")
         ret = putc ch
@@ -245,8 +247,16 @@ module RubyCurses
     # set cursor on correct column
     def set_form_col col1=@curpos
       @curpos = col1
-      #$log.debug " #{@orig_col}, #{@col_offset}. #{@curpos}."
+      cursor_bounds_check
       @form.col = @orig_col + @col_offset + @curpos
+      $log.debug "sfc: #{@orig_col}, #{@col_offset}. #{@curpos}. "
+    end
+    def cursor_bounds_check
+      max = buffer_len()
+      @curpos = max if @curpos > max # check 2008-12-27 00:02 
+    end
+    def buffer_len
+      @list[@prow].chomp().length  
     end
     def do_current_row # :yields current row
       yield @list[@prow]
@@ -260,17 +270,22 @@ module RubyCurses
       @list[@prow] = pos == -1 ? "" : @buffer[0..pos]
       $log.debug "delete EOL :pos=#{pos}, #{@delete_buffer}: row: #{@list[@prow]}:"
       @buffer = @list[@prow]
-      cursor_backward
+      cursor_backward if @curpos > 0 # now cursor back goes up to prev line
       #fire_handler :CHANGE, self  # 2008-12-09 14:56 
       fire_handler :CHANGE, InputDataEvent.new(@curpos,@curpos+@delete_buffer.length, self, :DELETE, @prow, @delete_buffer)     #  2008-12-24 18:34 
       return @delete_buffer
     end
     def cursor_forward
-      $log.debug "next char cp #{@curpos} wi: #{@width}"
-      if @curpos < @width and @curpos < @maxlen-1 # else it will do out of box
+      $log.debug "next char cp #{@curpos}, #{@buffer.length}. wi: #{@width}"
+      #if @curpos < @width and @curpos < @maxlen-1 # else it will do out of box
+      if @curpos < buffer_len()
         @curpos += 1
         addcol 1
+      else # trying this out 2008-12-26 20:18 
+        @curpos = 0
+        down
       end
+      cursor_bounds_check
     end
     def addcol num
       @form.addcol num
@@ -282,6 +297,9 @@ module RubyCurses
     if @curpos > 0
       @curpos -= 1
       addcol -1
+    else # trying this out 2008-12-26 20:18 
+      ret = up
+      cursor_eol if ret != -1
     end
   end
   def delete_line line=@prow
@@ -312,6 +330,9 @@ module RubyCurses
       set_modified 
       addcol -1
     end
+    # private
+    # when backspace pressed in position zero if the previous line is filled we may have to bring 
+    # down the last word and join, rather than go up
     def join_to_prev_line
       return -1 unless @editable
       return if @prow == 0
@@ -319,22 +340,52 @@ module RubyCurses
       oldprow = @prow
       prev = @list[@prow-1].chomp
       prevlen = prev.length
+      # 2008-12-26 21:37 delete previous line if nothing there. This moves entire buffer up.
+      if prevlen == 0
+        delete_line @prow-1
+        up
+        return
+      end
       space_left = @maxlen - prev.length
-      carry_up = @buffer[0..space_left]
-      @list[@prow-1]=prev + carry_up
-      space_left2 = @buffer[(space_left+1)..-1]
-      @list[@prow]=space_left2 #if !space_left2.nil?
-      @list[@prow] ||= ""
-      up
-      addrowcol -1,0
-      @curpos = prevlen
+      # BUG. carry full words up, or if no space then bring down last word of prev lien and join with first
+      carry_up = words_in_length @buffer, space_left #@buffer[0..space_left] # XXX
+      if carry_up.nil?
+        # carry down last word
+        prev_wd = remove_last_word @prow-1
+        @buffer.insert 0, prev_wd
+        @curpos = prev_wd.length
+        $log.debug " carry up nil! prev_wd (#{prev_wd}) len:#{prev_wd.length}"
+        fire_handler :CHANGE, InputDataEvent.new(0,prev_wd.length, self, :INSERT, oldprow, prev_wd)     #  2008-12-26 23:07 
+      else
+        $log.debug " carrying up #{carry_up.length} #{carry_up}, space: #{space_left}"
+        @list[@prow-1]=prev + carry_up
+        space_left2 = @buffer[(carry_up.length+1)..-1]
+        @list[@prow]=space_left2 #if !space_left2.nil?
+        @list[@prow] ||= ""
+        up
+        addrowcol -1,0
+        @curpos = prevlen
+        fire_handler :CHANGE, InputDataEvent.new(oldcurpos,carry_up.length, self, :DELETE, oldprow, carry_up)     #  2008-12-24 18:34 
+        fire_handler :CHANGE, InputDataEvent.new(prevlen,carry_up.length, self, :INSERT, oldprow-1, carry_up)     #  2008-12-24 18:34 
+      end
       @form.col = @orig_col + @col_offset + @curpos
-      fire_handler :CHANGE, InputDataEvent.new(oldcurpos,carry_up.length, self, :DELETE, oldprow, carry_up)     #  2008-12-24 18:34 
-      fire_handler :CHANGE, InputDataEvent.new(prevlen,carry_up.length, self, :INSERT, oldprow-1, carry_up)     #  2008-12-24 18:34 
 
 #     $log.debug "carry up: nil" if carry_up.nil?
 #     $log.debug "listrow nil " if @list[@prow].nil?
 #     $log.debug "carry up: #{carry_up} prow:#{@list[@prow]}"
+    end
+    ##
+    # return as many words as fit into len for carrying up..
+    # actually there is a case of when the next char (len+1) is a white space or word boundary. XXX
+    def words_in_length buff, len
+      return nil if len == 0
+      str = buff[0..len]
+      ix = str.rindex(/\s/)
+      $log.debug " str #{str} len #{len} ix #{ix} , buff #{buff}~"
+      return nil if ix.nil?
+      ix = ix > 0 ? ix - 1 : ix
+      $log.debug " str[]:#{str[0..ix]}~ len #{len} ix #{ix} , buff #{buff}~"
+      return str[0..ix]
     end
     def putch char
       return -1 if !@editable #or @buffer.length >= @maxlen
@@ -393,10 +444,11 @@ module RubyCurses
         # sometimme cursor is on a space and we;ve pushed it to next line
         # so cursor > buffer length
       end
-      @curpos = @buffer.length if @curpos > @buffer.length
+      $log.debug " inserting #{@buffer.length}, #{@curpos}, #{@buffer[-1]}, #{@buffer[-2]}, fc: #{@form.col}"
+      cursor_bounds_check
       @buffer.insert(@curpos, char)
       @curpos += 1 
-      addcol 1
+      #addcol 1 # 2008-12-27 00:25 
       @modified = true
       #fire_handler :CHANGE, self  # 2008-12-09 14:56 
       fire_handler :CHANGE, InputDataEvent.new(oldcurpos,@curpos, self, :INSERT, @prow, char)     #  2008-12-24 18:34 
@@ -405,6 +457,21 @@ module RubyCurses
     def append_row chars=""
         $log.debug "append row sapce:#{chars}."
       @list.insert @prow+1, chars
+    end
+    ##
+    # removes and returns last word in given line number, or nil if no whitespace
+    def remove_last_word lineno
+      @list[lineno].chomp!
+      line=@list[lineno]
+      lastspace = line.rindex(/\s/)
+      if !lastspace.nil?
+        lastchars = line[lastspace+1..-1]
+        @list[lineno].slice!(lastspace..-1)
+        $log.debug " remove_last: lastspace #{lastspace},#{lastchars},#{@list[lineno]}"
+        fire_handler :CHANGE, InputDataEvent.new(lastspace,lastchars.length, self, :DELETE, lineno, lastchars)     #  2008-12-26 23:06 
+        return lastchars
+      end
+      return nil
     end
 
     def putc c
@@ -424,12 +491,15 @@ module RubyCurses
       $log.debug "dele : #{@prow} #{@buffer} #{index}"
       char = @buffer.slice!(@curpos,1)  # changed added ,1 and take char for event
       # if no newline at end of this then bring up prev character/s till maxlen
+      # NO WE DON'T DO THIS ANYLONGER 2008-12-26 21:09 lets see
+=begin
       if @buffer[-1,1]!="\r"
         @buffer[-1]=" " if @buffer[-1,1]=="\n"
         if !next_line.nil? and next_line.length > 0
           move_chars_up
         end
       end
+=end
       #@modified = true 2008-12-22 15:31 
       set_modified true
       #fire_handler :CHANGE, self  # 2008-12-09 14:56 
@@ -456,12 +526,19 @@ module RubyCurses
     def next_line
       @list[@prow+1]
     end
+    def current_line
+      @list[@prow]
+    end
     def do_relative_row num
       yield @list[@prow+num] 
     end
     def set_modified tf=true
       @modified = tf
       @form.modified = true if tf
+    end
+    def cursor_eol
+      $log.error "ERROR !!! bufferlen gt maxlen #{@buffer.length}, #{@maxlen}" if @buffer.length > @maxlen
+      set_form_col current_line().chomp().length()-1
     end
   end # class textarea
   ##
@@ -630,7 +707,7 @@ module RubyCurses
     def set_form_row
       @form.row = @row + 1 + @winrow
     end
-    # set cursor on correct column
+    # set cursor on correct column tview
     def set_form_col col=@curpos
       @curpos = col
       @form.col = @orig_col + @col_offset + @curpos
