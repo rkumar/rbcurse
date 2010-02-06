c268f240bdab256edcd0cc5efed8ac9ec989e3cf
Rahul Kumar <rahulbenegal@gmail.com>
2010-02-06 23:53:53 +0530

    added RTEE LTEE corners, add_rows

--- lib/rbcurse/rtabbedpane.rb
+++ lib/rbcurse/rtabbedpane.rb
@@ -246,13 +246,18 @@ module RubyCurses
       form.navigation_policy = :NON_CYCLICAL
       window.bkgd(Ncurses.COLOR_PAIR($datacolor));
       window.box( 0, 0);
+      window.mvwaddch(0, 0, Ncurses::ACS_LTEE) # beautify the corner 2010-02-06 19:35 
+      window.mvwaddch(0, @width-1, Ncurses::ACS_RTEE)
+
       ## this prints the tab name on top left
       window.mvprintw(1,1, tab.text.tr('&', ''))
       window.name = "TAB-#{tab.text}" # 2010-02-02 19:59 
       ##window.wrefresh
       ##Ncurses::Panel.update_panels
       form.add_cols=@col-mleft # added 2010-01-26 20:25 since needs accounting by cursor
-      form.add_rows=@row-mtop # added 2010-01-26 20:25 since needs accounting by cursor
+      # reducing mtop causes a problem in cursor placement if scrollpane placed inside.
+      # in testtpane2.rb i've had to add it back, temporarily.
+      form.add_rows=@row -mtop # added 2010-01-26 20:25 since needs accounting by cursor
       return form
     end
     ##
