4b4f6013bace5d55aaa16ee0fb44bf48ca36a5d5
Rahul Kumar <rahulbenegal@gmail.com>
2010-05-23 12:26:45 +0530

    key mappings for listboxes, and fire_handlers so undo/redo can happen

--- lib/rbcurse/vieditable.rb
+++ lib/rbcurse/vieditable.rb
@@ -12,13 +12,14 @@ require 'rbcurse/listeditable'
 module ViEditable
   include ListEditable
 
-  #def ViEditable.vieditable_init
   def vieditable_init
     $log.debug " inside vieditable_init "
     @editable = true
     bind_key( ?C, :edit_line)
-    bind_key( ?o, :insert_line)
-    bind_key( ?O) { insert_line(@current_index-1) } 
+    #bind_key( ?o, :insert_line)
+    #bind_key( ?O) { insert_line(@current_index-1) } 
+    bind_key( ?o) { insert_line(@current_index+1) } 
+    bind_key( ?O) { insert_line(@current_index) } 
     bind_key( ?D, :delete_eol)
     bind_key( [?d, ?$], :delete_eol)
     bind_key( [?d, ?d] , :delete_line ) 
@@ -35,25 +36,64 @@ module ViEditable
     bind_key( ?P ) { yank(@current_index - 1) } # should be before this line
     bind_key(?\w, :forward_word)
     bind_key(?f, :forward_char)
+    bind_key(?\M-y, :yank_pop)
+    bind_key(?\M-w, :kill_ring_save)
+
+  end
+  ##
+  # Separate mappings for listboxes.
+  # Some methods don;'t make sense for listboxes and are crashing
+  # since not present for them. f was being overwritten, too.
+  # Sorry for duplication, need to clean this somehow.
+  def vieditable_init_listbox
+    $log.debug " inside vieditable_init_listbox "
+    @editable = true
+    bind_key( ?C, :edit_line)
+    bind_key( ?o) { insert_line(@current_index+1) } 
+    bind_key( ?O) { insert_line(@current_index) } 
+    #bind_key( ?D, :delete_eol)
+    #bind_key( [?d, ?$], :delete_eol)
+    bind_key( [?d, ?d] , :delete_line ) 
+    #bind_key( [?d, ?w], :delete_word )
+    #bind_key( [?d, ?t], :delete_till )
+    #bind_key( [?d, ?f], :delete_forward )
+    bind_key( ?\C-_ ) { @undo_handler.undo if @undo_handler }
+    bind_key( ?u ) { @undo_handler.undo if @undo_handler }
+    bind_key( ?\C-r ) { @undo_handler.redo if @undo_handler }
+    #bind_key( ?x, :delete_curr_char )
+    #bind_key( ?X, :delete_prev_char )
+    bind_key( [?y, ?y] , :kill_ring_save ) 
+    bind_key( ?p, :yank ) # paste after this line
+    bind_key( ?P ) { yank(@current_index - 1) } # should be before this line
+    bind_key(?\w, :forward_word)
+    #bind_key(?f, :forward_char)
+    bind_key(?\M-y, :yank_pop)
+    bind_key(?\C-y, :yank)
+    bind_key(?\M-w, :kill_ring_save)
 
   end
 
   ##
   # edit current or given line
+  # FIXME needs to fire handler 2010-05-23 11:40 
   def edit_line lineno=@current_index
     line = @list[lineno]
     prompt = "Edit: "
     maxlen = 80
     config={}; 
+    oldline = line.dup
     config[:default] = line
     ret, str = rbgetstr(@form.window, $error_message_row, $error_message_col,  prompt, maxlen, config)
     $log.debug " rbgetstr returned #{ret} , #{str} "
     return if ret != 0
     @list[lineno].replace(str)
+    fire_handler :CHANGE, InputDataEvent.new(0,oldline.length, self, :DELETE_LINE, lineno, oldline)     #  2008-12-24 18:34 
+    fire_handler :CHANGE, InputDataEvent.new(0,str.length, self, :INSERT_LINE, lineno, str)
     @repaint_required = true
   end
   ##
   # insert a line 
+  # FIXME needs to fire handler 2010-05-23 11:40 
   def insert_line lineno=@current_index
     prompt = "Insert: "
     maxlen = 80
@@ -65,6 +105,8 @@ module ViEditable
     $log.debug " rbgetstr returned #{ret} , #{str} "
     return if ret != 0
     @list.insert lineno, str
+    ## added handler on 2010-05-23 11:46 - undo works - tested in testlistbox.rb
+    fire_handler :CHANGE, InputDataEvent.new(0,str.length, self, :INSERT_LINE, lineno, str)
     @repaint_required = true
   end
   ##
