989a2b7a047bea484b681f4f90495bac043984ed
Rahul Kumar <rahulbenegal@gmail.com>
2010-08-23 20:56:05 +0530

    added LOGDIR to view.log

--- examples/testchars.rb
+++ examples/testchars.rb
@@ -11,7 +11,7 @@ if $0 == __FILE__
   begin
   # Initialize curses
     VER::start_ncurses  # this is initializing colors via ColorMap.setup
-    $log = Logger.new("view.log")
+    $log = Logger.new((File.join(ENV["LOGDIR"] || "./" ,"view.log")))
     $log.level = Logger::DEBUG
 
     @window = VER::Window.root_window
