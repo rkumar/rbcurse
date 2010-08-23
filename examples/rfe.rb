4710079ffdfe455e71837021c135ea76ee7124e4
Rahul Kumar <rahulbenegal@gmail.com>
2010-08-23 13:42:06 +0530

    check dir exists

--- examples/rfe.rb
+++ examples/rfe.rb
@@ -998,7 +998,9 @@ if $0 == __FILE__
   begin
     # Initialize curses
     VER::start_ncurses  # this is initializing colors via ColorMap.setup
-    $log = Logger.new("view.log")
+    #$log = Logger.new("view.log")
+    $log = Logger.new(ENV['LOGDIR'] || "" + "view.log")
+
     $log.level = Logger::DEBUG
 
     catch(:close) do
