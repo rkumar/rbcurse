1fa49687d0b86e5c4a827add345cc433b3cf622a
Rahul Kumar <rahulbenegal@gmail.com>
2010-09-01 16:22:07 +0530

    directory listing added

--- examples/testmultispl.rb
+++ examples/testmultispl.rb
@@ -8,7 +8,62 @@ require 'rbcurse/rlistbox'
 #
 ## this sample creates a multisplitpane with n objects
 ##+ and move divider around using - + and =.
+# ON_LEAVE make title normal
+# if possible selection should also become dimmer
+# Stuatus bar below to reflect full path.
 #
+KEY_ENTER = 13
+# @param [Listbox] new list to bind
+# @param [Array] array of lists
+# @param [MultiSplit] msp object to add a new list to
+def bind_list(listb, lists, splitp)
+  #listb.bind(:ENTER_ROW, mylist) {|lb,list| row_cmd.call(lb,list) }
+  listb.bind_key(KEY_ENTER) {     
+    #@status_row.text = "Selected #{tablelist.get_content()[tablelist.current_index]}"
+    item = "#{listb.get_content()[listb.current_index]}"
+    fullname = File.join(listb.config[:path], item)
+    mylist = nil
+    if File.directory? fullname
+      $log.debug " DIRector is a item #{item} "
+      d = Dir.new(fullname)
+      mylist = d.entries
+      mylist.delete "."
+      mylist.delete ".."
+      $log.debug "1 MYLIST #{mylist} dir #{item} "
+      if lists.empty? || lists.last == listb
+        listc = Listbox.new @form do
+          name  "LIST" 
+          list mylist
+          title item
+          title_attrib 'reverse'
+        end
+        listc.one_key_selection = false
+        listc.bind(:ENTER) {|l| l.title_attrib 'reverse';  }
+        listc.bind(:LEAVE) {|l| l.title_attrib 'normal';  }
+        splitp.add listc
+        lists << listc
+        listb.config[:child] = listc
+        listc.config[:path] = File.join(listb.config[:path], item)
+        bind_list(listc, lists, splitp)
+      else
+        #l = lists.first
+        l = listb.config[:child]
+        l.list_data_model.remove_all
+        l.list_data_model.insert 0, *mylist
+        l.title = item
+        while true
+          n = l.config[:child]
+          break if n.nil?
+          n.list_data_model.remove_all
+          # TODO should be removed, so focus does not go to it
+          n.title = ""
+          l = n
+        end
+        $log.debug " MYLIST #{mylist} dir #{item} "
+      end
+    end
+  } 
+end
 if $0 == __FILE__
   include RubyCurses
   include RubyCurses::Utils
@@ -16,7 +71,8 @@ if $0 == __FILE__
   begin
   # Initialize curses
     VER::start_ncurses  # this is initializing colors via ColorMap.setup
-    $log = Logger.new("view.log")
+    $log = Logger.new((File.join(ENV['LOGDIR'] || "./" ,"view.log")))
+
     $log.level = Logger::DEBUG
 
     @window = VER::Window.root_window
@@ -51,6 +107,24 @@ if $0 == __FILE__
         end
       splitp.bind(:PROPERTY_CHANGE){|e| $message.value = e.to_s }
 
+      lists = []
+      mylist = Dir.glob('*')
+      listb = Listbox.new @form do
+        name   "mylist" 
+        list mylist
+        title "A short list"
+        title_attrib 'reverse'
+      end
+      listb.config[:path] = Dir.getwd
+      listb.one_key_selection = false
+      splitp.add listb
+      row_cmd = lambda {|lb, list| file = list[lb.current_index]; $message.value = file; # File.stat("#{cur_dir()}/#{file}").inspect 
+      }
+      listb.bind(:ENTER_ROW, mylist) {|lb,list| row_cmd.call(lb,list) }
+      listb.bind(:ENTER) {|l| l.title_attrib 'reverse';  }
+      listb.bind(:LEAVE) {|l| l.title_attrib 'normal';  }
+      bind_list(listb, lists, splitp)
+
       @form.repaint
       @window.wrefresh
       Ncurses::Panel.update_panels
@@ -58,7 +132,8 @@ if $0 == __FILE__
       while((ch = @window.getchar()) != ?q.getbyte(0) )
         str = keycode_tos ch
         case ch
-        when ?a.getbyte(0)
+        when ?A.getbyte(0)
+          next
           r = 0
         mylist = []
         counter.upto(counter+100) { |v| mylist << "#{v} scrollable data" }
@@ -66,17 +141,13 @@ if $0 == __FILE__
         $listdata = Variable.new mylist
         listb = Listbox.new @form do
           name   "mylist" 
-          #row  r 
-          #col  0 
-          #width 40
-          #height 11
 #         list mylist
           list_variable $listdata
           selection_mode :SINGLE
           #show_selector true
           #row_selected_symbol "[X] "
           #row_unselected_symbol "[ ] "
-          title "A long list"
+          title "A short list"
           title_attrib 'reverse'
           #cell_editing_allowed true
         end
@@ -84,18 +155,18 @@ if $0 == __FILE__
         #$listdata.value.insert 55, "hello ruby", "so long python", "farewell java", "RIP .Net"
         #listb.list_data_model.insert 55, "hello ruby", "so long python", "farewell java", "RIP .Net", "hi lisp", "hi clojure"
         splitp.add listb
-        when ?v.getbyte(0)
+        when ?V.getbyte(0)
           splitp.orientation(:VERTICAL_SPLIT)
-          splitp.reset_to_preferred_sizes
-        when ?h.getbyte(0)
+          #splitp.reset_to_preferred_sizes
+        when ?H.getbyte(0)
           splitp.orientation(:HORIZONTAL_SPLIT)
-          splitp.reset_to_preferred_sizes
+          #splitp.reset_to_preferred_sizes
         when ?-.getbyte(0)
-          splitp.set_divider_location(splitp.divider_location-1)
+          #splitp.set_divider_location(splitp.divider_location-1)
         when ?+.getbyte(0)
-          splitp.set_divider_location(splitp.divider_location+1)
+          #splitp.set_divider_location(splitp.divider_location+1)
         when ?=.getbyte(0)
-          splitp.set_resize_weight(0.50)
+          #splitp.set_resize_weight(0.50)
         end
         #splitp.get_buffer().wclear
         #splitp << "#{ch} got (#{str})"
@@ -114,3 +185,4 @@ if $0 == __FILE__
     $log.debug(ex.backtrace.join("\n")) if ex
   end
 end
+
