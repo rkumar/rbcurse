1db121eb76d73fb3cb7698185ed0a80d72026c52
Rahul Kumar <rahulbenegal@gmail.com>
2010-09-11 16:21:29 +0530

    rinputdataevent.rb: added getvalue to inputdataevent

--- lib/rbcurse/rinputdataevent.rb
+++ lib/rbcurse/rinputdataevent.rb
@@ -8,6 +8,10 @@
   * License:
     Same as Ruby's License (http://www.ruby-lang.org/LICENSE.txt)
 
+NOTE: this is how we used to write code in the Java days. Anyone reading this source,
+this is NOT how to code in rubyland. Please see this link, for how to code such as class:
+http://blog.grayproductions.net/articles/all_about_struct
+
 =end
 require 'rubygems'
 
@@ -24,6 +28,8 @@ module RubyCurses
       @row = row
       @text = text
     end
+    # until now to_s was returning inspect, but to make it easy for users let us return the value
+    # they most expect which is the text that was changed
     def to_s
       inspect
     end
@@ -31,6 +37,12 @@ module RubyCurses
       ## now that textarea.to_s prints content we shouldn pass it here.
       #"#{@type.to_s}, #{@source}, ind0:#{@index0}, ind1:#{@index1}, row:#{@row}, text:#{@text}"
       "#{@type.to_s}, ind0:#{@index0}, ind1:#{@index1}, row:#{@row}, text:#{@text}"
+    end
+    # this is so that earlier applications were getting source in the block, not an event. they 
+    # were doing a fld.getvalue, so we must keep those apps running
+    # @since 1.2.0  added 2010-09-11 12:25 
+    def getvalue
+      @source.getvalue
     end
   end
 end
