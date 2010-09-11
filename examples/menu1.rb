27f39a44723e9e03b0cf6905eb2727f767c913f1
Rahul Kumar <rahulbenegal@gmail.com>
2010-09-11 23:02:33 +0530

    menu1.rb added blank and horiz line

--- examples/menu1.rb
+++ examples/menu1.rb
@@ -36,17 +36,28 @@ if $0 == __FILE__
     mb.toggle_key = KEY_F2
     @form.set_menu_bar mb
     stack :margin_top => 10, :margin => 5 do
-      field "a field", :attr => 'reverse', :block_event => :CHANGE do |fld|
-        case fld.getvalue
+      field "a field", :attr => 'reverse', :block_event => :CHANGE do |e|
+        message e.to_s
+
+        case e.text
         when "d"
-          alert("Me gots #{fld.getvalue} disabling menu item Window:Find: #{$x} ")
-          $x.enabled = false
+          #alert("Me gots #{e.text} disabling menu item Window:Find: ")
+          #$x.enabled = false
         when "e"
-          alert("Me gots #{fld.getvalue} enabling menubar:Window:Less and setting Accelerator to C-x")
-          $x.enabled = true
+          # TODO this alert shows data wrapped but overlaps border
+          #alert("Me gots #{e.text} enabling menu item Window:Find: Adding accel ")
+          #$x.enabled = true
           $x.accelerator = "Ctrl-X"
         end
       end
-    end
+      lbltext = "Click this to enable or disable menu option Window:Find:Less"
+      blank 1
+      hline :width => lbltext.length
+      label :text => lbltext
+      toggle :offvalue => " Enable  ", :onvalue => " Disable ", :mnemonic => 'E', :value => true do |e|
+        $x.enabled = e.item.value
+        @form.handle_key mb.toggle_key
+      end
+    end # stack
   end # app
 end
