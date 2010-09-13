2211fa19593d37624fcbc749f1a6f52466654742
Rahul Kumar <rahulbenegal@gmail.com>
2010-09-13 12:12:11 +0530

    alpmenu.rb: calling viewtodo and testtodo from demo menu

--- examples/alpmenu.rb
+++ examples/alpmenu.rb
@@ -5,12 +5,23 @@ if $0 == __FILE__
     #title "Demo of Menu - rbcurse"
     #subtitle "Hit F1 to quit, F2 for menubar toggle"
     header = app_header "rbcurse 1.2.0", :text_center => "Alpine Menu Demo", :text_right =>""
+    message_row(27)
 
     # TODO accelerators and 
     # getting a handle for later use
     stack :margin_top => 10, :margin => 15 do
       #w = "Messages".length + 1
       w = 60
+      menulink "&View Todo", :width => w, :description => "View TODO in sqlite"  do |s|
+        message "Pressed #{s.text} "
+        require './viewtodo'; todo = ViewTodo::TodoApp.new; todo.run
+      end
+      blank
+      menulink "&Edit Todo", :width => w, :description => "Edit TODO in CSV"  do |s|
+        message "Pressed #{s.text} "
+        require './testtodo'; todo = TestTodo::TodoApp.new; todo.run
+      end
+      blank
       menulink "&Messages", :width => w, :description => "View messages in current folder"  do |s|
         message "Pressed #{s.text} "
       end
@@ -21,6 +32,10 @@ if $0 == __FILE__
       blank
       menulink "Setup", :mnemonic => "?", :width => w, :description => "Configure Alpine options"  do |s|
         message "Pressed #{s.text} "
+      end
+      blank
+      menulink "&Quit", :width => w, :description => "Quit this application"  do |s|
+        quit
       end
       @form.bind(:ENTER) do |w|
         header.text_right = w.text
