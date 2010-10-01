a6d85adc3572131bb9ed1f1a662b62d554138f20
Rahul Kumar <rahulbenegal@gmail.com>
2010-10-01 13:31:46 +0530

    comboboxcellrenderer.rb removed init_var since overriding and not adding value

--- lib/rbcurse/comboboxcellrenderer.rb
+++ lib/rbcurse/comboboxcellrenderer.rb
@@ -12,13 +12,6 @@ module RubyCurses
   class ComboBoxCellRenderer < ListCellRenderer
     include ConfigSetup
     include RubyCurses::Utils
-    dsl_accessor :justify     # :right, :left, :center  # added 2008-12-22 19:02 
-    dsl_accessor :display_length     #  please give this to ensure the we only print this much
-    dsl_accessor :height    # if you want a multiline label.
-    dsl_accessor :text    # text of label
-    dsl_accessor :color, :bgcolor
-    dsl_accessor :row, :col
-    dsl_accessor :parent    #usuall the table to get colors and other default info
 
     def initialize text="", config={}, &block
       @text = text
@@ -27,10 +20,6 @@ module RubyCurses
       config_setup config # @config.each_pair { |k,v| variable_set(k,v) }
       instance_eval &block if block_given?
       init_vars
-    end
-    def init_vars
-      @justify ||= :left
-      @display_length ||= 10
     end
     ## me thinks this is unused
     def getvalue
