2d95913b1fd597d9431be30e318216583c393cf8
Rahul Kumar <rahulbenegal@gmail.com>
2011-09-08 00:04:50 +0530

    FFI mvprintw

--- lib/ver/window.rb
+++ lib/ver/window.rb
@@ -2,7 +2,7 @@ require 'ver/ncurses'
 require 'ver/panel'
 module Ncurses # added FFI 2011-09-6 
   # results in a stack overflow
-  def _method_missing(meth, *args)
+  def method_missing(meth, *args)
     #      $log.debug " WWWW method missing #{meth} "
           if meth[0,1]=="w" || meth[0,3] == "mvw"
     #        $log.debug " WWWW method missing #{meth} adding window in call "
