1899e04946586840cadd37311e0d8c7f88d26f56
Rahul Kumar <rahulbenegal@gmail.com>
2011-09-07 23:07:25 +0530

    FFI mvprintw

--- lib/ver/window.rb
+++ lib/ver/window.rb
@@ -2,9 +2,9 @@ require 'ver/ncurses'
 require 'ver/panel'
 module Ncurses # added FFI 2011-09-6 
     def method_missing(meth, *args)
-      $log.debug " WWWW method missing #{meth} "
+      #$log.debug " WWWW method missing #{meth} "
       if meth[0,1]=="w" || meth[0,3] == "mvw"
-        $log.debug " WWWW method missing #{meth} adding window in call "
+        #$log.debug " WWWW method missing #{meth} adding window in call "
         return FFI::NCurses.send(meth, @window, *args)
       else
       end
