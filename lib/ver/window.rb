a567b846aad2342827597882443822d94a33398a
Rahul Kumar <rahulbenegal@gmail.com>
2011-09-08 11:51:45 +0530

    FFI delwin

--- lib/ver/window.rb
+++ lib/ver/window.rb
@@ -341,7 +341,8 @@ module VER
       #@panel = @window.panel if @window
       # changed Ncurses::Panel to VER::Panel on 2011-09-8 when moved to FFI
       VER::Panel.del_panel(@panel.pointer) if !@panel.nil?    # ADDED FFI pointer 2011-09-7 
-      @window.delwin(@window) if !@window.nil? # added FFI 2011-09-7 
+      #@window.delwin(@window) if !@window.nil? # added FFI 2011-09-7 
+      delwin(@window) if !@window.nil? # added FFI 2011-09-7 
       $log.debug "win destroy end"
     end
     ## 
