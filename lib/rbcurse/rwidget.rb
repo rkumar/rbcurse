446cbd601528d47dc4c4654876c0c1c825f69f66
Rahul Kumar <rahulbenegal@gmail.com>
2011-09-08 12:15:19 +0530

    FFI added module KeysFFI

--- lib/rbcurse/rwidget.rb
+++ lib/rbcurse/rwidget.rb
@@ -114,12 +114,6 @@ end unless "a"[0] == "a"
 #include Ncurses XXX 2011-09-8 testing FFI
 module RubyCurses
   extend self
-    # testing FFI since we've used KEYS without qualifying module
-    def const_missing name
-      val = FFI::NCurses.const_get(name)
-      #const_set(name, val)
-      return val
-    end
   include ColorMap
     class FieldValidationException < RuntimeError
     end
