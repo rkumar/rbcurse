f17de9f970b7c79c82266ed41e2883e274a12da2
Rahul Kumar <rahulbenegal@gmail.com>
2011-09-08 16:26:10 +0530

    FFI color

--- lib/rbcurse/colormap.rb
+++ lib/rbcurse/colormap.rb
@@ -6,7 +6,7 @@ module ColorMap
   def ColorMap.get_color_const colorstring
     ret = FFI::NCurses.const_get "COLOR_#{colorstring.upcase}"
     raise  "color const nil ColorMap 8 " if !ret
-    raise "GOT #{ret} COLORMAP " if ret > 0
+    $log.debug "GOT #{ret} COLORMAP " if ret > 0
     ret
   end
   ## private
