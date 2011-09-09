59f631bfd2b763875b91bb0d9f45a2cbdbcd858b
Rahul Kumar <rahulbenegal@gmail.com>
2011-09-09 14:03:53 +0530

    FFI bottomline stdscr pointer getch

--- lib/rbcurse/extras/stdscrwindow.rb
+++ lib/rbcurse/extras/stdscrwindow.rb
@@ -14,7 +14,7 @@ module RubyCurses
 
     def initialize
 
-      @window_pointer = FFI::NCurses.stdscr
+      @window_pointer = FFI::NCurses.initscr
       $log.debug "STDSCR window pointer is #{@window_pointer.class}"
       $log.debug "STDSCR window pointer mehtods #{@window_pointer.public_methods}"
       $error_message_row ||= Ncurses.LINES-1
