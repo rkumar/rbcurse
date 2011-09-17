48f0e5d16a371eae62b347929751b3488ef83373
Rahul Kumar <rahulbenegal@gmail.com>
2011-09-17 23:28:48 +0530

    viewer.rb reduced textview height by 2

--- lib/rbcurse/extras/viewer.rb
+++ lib/rbcurse/extras/viewer.rb
@@ -3,6 +3,7 @@ require 'fileutils'
 
 # NOTE: experimental, not yet firmed up
 # If you use in application, please copy to some application folder in case i change this.
+# Can be used for print_help_page
 # TODO: add vi_keys here
 # SUGGESTIONS WELCOME.
 # @since 1.2.0
@@ -11,6 +12,8 @@ module RubyCurses
   # view filename, :close_key => KEY_RETURN
   # send data in an array
   # view Array, :close_key => KEY_RETURN, :layout => [0,0,23,80]
+  # when passing layout reserve 4 rows for window and border. So for 2 lines of text
+  # give 6 rows.
   class Viewer
     # @param filename as string or content as array
     # @yield textview object for further configuration before display
@@ -44,7 +47,7 @@ module RubyCurses
         row  0
         col  0
         width ww
-        height wh-2
+        height wh-0 # earlier 2 but seems to be leaving space.
         title fp
         title_attrib ta
         print_footer pf
