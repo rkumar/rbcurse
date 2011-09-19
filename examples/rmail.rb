cd33be1270a54f4c5c9a6ed44808fb4e5e4adbf4
Rahul Kumar <rahulbenegal@gmail.com>
2011-09-19 13:38:47 +0530

    added mailread inside since its not in github

--- examples/rmail.rb
+++ examples/rmail.rb
@@ -4,7 +4,7 @@
 # supply the path of any mbox file such as ~/mbox or ~/mail/read-mail
 # and see a listing of mails, then give a msg number and see body
 # You may run this with -EASCII-8BIT if it crashes with a UTF8 error.
-require 'mailread'
+#require 'mailread' # i've copied it in here 2011-09-18 
 require 'date'
 
 #BOLD       = "\e[1m"
@@ -161,6 +161,74 @@ class MboxManager
   def mails folder=@current_name
     use folder
     @mails
+  end
+end
+
+# The Mail class represents an internet mail message (as per RFC822, RFC2822)
+# with headers and a body. 
+class Mail
+
+  # Create a new Mail where +f+ is either a stream which responds to gets(),
+  # or a path to a file.  If +f+ is a path it will be opened.
+  #
+  # The whole message is read so it can be made available through the #header,
+  # #[] and #body methods.
+  #
+  # The "From " line is ignored if the mail is in mbox format.
+  def initialize(f)
+    unless defined? f.gets
+      f = open(f, "r")
+      opened = true
+    end
+
+    @header = {}
+    @body = []
+    begin
+      while line = f.gets()
+        line.chop!
+        # Added encode by RK since crashing with UTF-8 error
+        line = line.encode("ASCII-8BIT", :invalid => :replace, :undef => :replace, :replace => "?")
+        next if /^From /=~line	# skip From-line
+        break if /^$/=~line	# end of header
+
+        if /^(\S+?):\s*(.*)/=~line
+          (attr = $1).capitalize!
+          @header[attr] = $2
+        elsif attr
+          line.sub!(/^\s*/, '')
+          @header[attr] += "\n" + line
+        end
+      end
+
+      return unless line
+
+      while line = f.gets()
+        # Added encode by RK since crashing with UTF-8 error
+        line = line.encode("ASCII-8BIT", :invalid => :replace, :undef => :replace, :replace => "?")
+        #line = line.encode('ASCII-8BIT') # added RK
+        break if /^From /=~line
+        @body.push(line)
+      end
+    ensure
+      f.close if opened
+    end
+  end
+
+  # Return the headers as a Hash.
+  def header
+    return @header
+  end
+
+  # Return the message body as an Array of lines
+  def body
+    return @body
+  end
+
+  # Return the header corresponding to +field+. 
+  #
+  # Matching is case-insensitive.
+  def [](field)
+    @header[field.capitalize]
   end
 end
 #puts mails.size
