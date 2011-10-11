826a98e7d81928d000e6a67bd192df6185ba82cb
Rahul Kumar <rahulbenegal@gmail.com>
2011-10-11 19:22:57 +0530

    listkeys.rb row selector for listbox by default is 0 (c_space) since editable

--- lib/rbcurse/listkeys.rb
+++ lib/rbcurse/listkeys.rb
@@ -4,7 +4,7 @@ module RubyCurses
     # That was possible earlier, but now that i am binding the key at construction time
     # any changes to the vars after construction won't have an effect.
     def install_list_keys
-#      @KEY_ROW_SELECTOR ||= ?\C-x.getbyte(0)  # need to changed from C-x since used for actions # changed 2011 dts  
+      #@KEY_ROW_SELECTOR ||= ?\C-x.getbyte(0)  # need to changed from C-x since used for actions # changed 2011 dts  
       @KEY_ROW_SELECTOR ||= 0  # need to changed from C-x since used for actions
       @KEY_BLOCK_SELECTOR ||= ?\M-x.getbyte(0)  # need to change since M-x used for commands
       @KEY_GOTO_TOP ||= ?\M-0.getbyte(0)
