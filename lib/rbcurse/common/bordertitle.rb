be73802cf4ad37e912b17674623848418549ca34
Rahul Kumar <rahulbenegal@gmail.com>
2011-10-29 00:06:14 +0530

    bordertitle.rb taken from textview

--- lib/rbcurse/common/bordertitle.rb
+++ lib/rbcurse/common/bordertitle.rb
@@ -5,24 +5,35 @@ module BorderTitle
     dsl_accessor :border_attrib, :border_color
     dsl_accessor :title                       #set this on top
     dsl_accessor :title_attrib                #bold, reverse, normal
+    dsl_accessor :border_attrib, :border_color # 
+
     def init
       @row_offset = @col_offset = 0 if @suppress_borders 
       @internal_width = 1 if @suppress_borders
     end
     def print_borders
       width = @width
-      height = @height-1 # 2010-01-04 15:30 BUFFERED HEIGHT
-      window = @graphic  # 2010-01-04 12:37 BUFFERED
+      height = @height-1
+      window = @graphic
       startcol = @col 
       startrow = @row 
       @color_pair = get_color($datacolor)
-      #$log.debug "rlistb #{name}: window.print_border #{startrow}, #{startcol} , h:#{height}, w:#{width} , @color_pair, @attr "
-      window.print_border startrow, startcol, height, width, @color_pair, @attr
+      bordercolor = @border_color || @color_pair
+      borderatt = @border_attrib || Ncurses::A_NORMAL
+      window.print_border startrow, startcol, height, width, bordercolor, borderatt
       print_title
     end
     def print_title
-      $log.debug "RCONTAINER PRINTING TITLE at #{row} #{col} "
-      @graphic.printstring( @row, @col+(@width-@title.length)/2, @title, @color_pair, @title_attrib) unless @title.nil?
+      return unless @title
+      raise "#{self} needs width" unless @width
+      @color_pair ||= get_color($datacolor) # should we not use this ??? XXX 
+      #$log.debug " print_title #{@row}, #{@col}, #{@width}  "
+      # check title.length and truncate if exceeds width
+      _title = @title
+      if @title.length > @width - 2
+        _title = @title[0..@width-2]
+      end
+      @graphic.printstring( @row, @col+(@width-_title.length)/2, _title, @color_pair, @title_attrib) unless @title.nil?
     end
 
 end
