3c8ffa188e9aed1231189af178a150f26d7b6c38
Rahul Kumar <rahulbenegal@gmail.com>
2011-11-03 14:56:48 +0530

    stackflow.rb moved a lot of stuff to BaseStack

--- lib/rbcurse/extras/stackflow.rb
+++ lib/rbcurse/extras/stackflow.rb
@@ -10,26 +10,30 @@ NOTE: Still experimental
   * Date:  23.10.11 - 19:55
   * License: Same as Ruby's License (http://www.ruby-lang.org/LICENSE.txt)
 
-  * Last update:  29.10.11 - 18:13
+  * Last update:  30.10.11 - 12:55
 
   == CHANGES
+     Have moved most things out to a module ModStack, so this is sort of just 
+     a skeletal container
     x take care of margins
     Resizing components
     If window or container resized then redo the calc again.
     Flow to have option of right to left orientation
   == TODO 
+    - If user specifies width, height then to be accounted when calculating weight. Also,
+      in such cases we must try not to overwrite h/w when calculating.
     - changing an objects config not easy since it is stored in item, user may not have
       handle to item
     - weightx weighty
     - RESET height only if expandable
     - exceeding 100 will result in exceeding container.
-    - are two weights needed horiz and vertical ?
     - C-a C-e misbehaving in examples
 
 =end
 
 require 'rbcurse'
 require 'rbcurse/common/bordertitle'
+require 'rbcurse/common/basestack'
 
 include RubyCurses
 module RubyCurses
@@ -43,6 +47,7 @@ module RubyCurses
   class StackFlow < Widget
 
     include BorderTitle
+    include ModStack
     # should container stack objects ignoring users row col
     # this is esp needed since App sets row and col which is too early
     # This is now the default value, till i can redo things
@@ -74,12 +79,12 @@ module RubyCurses
       bind_key(?\M-1, :increase_current)
       bind_key(?\M-2, :decrease_current)
       #raise "NO components !" if @components.empty?
-      #@components.each { |e| calc_weightages e }
-      calc_weightages2(@components, self)
+      calc_weightages2(@components, self) # FIXME this needs to move to basestack
 
     end
 
 
+    # NOTE this is called by basestack so it cannot be here FIXME
 
     # NOTE: since we are handling the traversal, we delink the object from any
     # form's widgets array  that might have been added. Whenever a form is available,
@@ -218,36 +223,6 @@ module RubyCurses
         $log.warn "XXX: WARN #{e.class} is out of bounds col #{e.col} + w #{e.width} >= #{@width} "
         #e.visible = false
       end
-    end
-    # Traverses the comopnent tree and calculates weightages for all components
-    # based on what has been specified by user
-    def calc_weightages2 components, parent
-        #puts " #{@ctr} --> #{c.type}, wt: #{c.config[:weight]} "
-        @ctr += 1
-        wt  = 0
-        cnt = 0
-        sz = components.count
-        $log.debug "XXX: calc COMP COUNT #{sz} "
-        # calculate how much weightage has been given by user
-        # so we can allocate average to other components
-        components.each { |e| 
-          if e.config[:weight]  
-            wt += e.config[:weight]
-            cnt += 1
-          end
-          $log.debug "XXX: INC setting parent #{parent} to #{e} "
-          e.config[:parent] = parent
-          e.config[:level] = @ctr
-        }
-        used = sz - cnt
-        $log.debug "XXX: ADDING calc COMP COUNT #{sz} - #{cnt} "
-        if used > 0
-          avg = (100-wt)/used
-          # Allocate average to other components
-          components.each { |e| e.config[:weight] = avg unless e.config[:weight]  }
-        end
-        components.each { |e| calc_weightages2(e.components, e) if e.respond_to? :components  }
-        @ctr -= 1
     end
 
     public
@@ -435,65 +410,6 @@ module RubyCurses
       set_form_row
     end
 
-    # General routin to traverse components and their components
-    def traverse c, &block
-      if c.is_a? BaseStack
-        yield c
-        c.components.each { |e| 
-          yield e
-        }
-        c.components.each { |e| traverse(e, &block)  }
-        @ctr -= 1
-      else
-      end
-    end
-
-    # traverse the components and their children
-    #
-    def each &block
-      @components.each { |e| traverse e, &block }
-    end
-    private
-    def _stack type, config={}, &block
-      case type
-      when :stack
-        s = Stack.new(config)
-      when :flow
-        s = Flow.new(config)
-      end
-      _add s
-      @active << s
-      yield_or_eval &block if block_given? 
-      @active.pop
-    end
-    private
-    def _add s
-      if @active.empty?
-        $log.debug "XXX:  ADDING TO components #{s} "
-        unless s.is_a? BaseStack
-          raise "No stack or flow to add to. Results may not be what you want"
-        end
-        @components << s
-      else
-        @active.last.components << s
-      end
-      __add s
-    end
-
-    public
-    def stack config={}, &block
-      $log.debug "XXX:  ADDING STACK "
-      _stack :stack, config, &block
-    end
-    def flow config={}, &block
-      _stack :flow, config, &block
-    end
-    def add w, config={}
-      i = Item.new config, w
-      _add i
-    end
-    alias :add_widget :add
-
     def increase_current
       c = @current_component
       p = c.config[:parent]
@@ -506,300 +422,9 @@ module RubyCurses
       $log.debug "XXX: INC increase current #{c} , #{p} "
       p.decrease c
     end
-    # given an widget, return the item, so we can change weight or some other config
-    def item_for widget
-      each do |e|
-        if e.is_a? Item
-          if e.widget == widget
-            return e
-          end
-        end
-      end
-      return nil
-    end
-    # returns the parent (flow or stack) for a given widget
-    #  allowing user to change configuration such as weight
-    def parent_of widget
-      f = item_for widget
-      return f.config[:parent] if f
-      return nil
-    end
     # ADD HERE ABOVe
   end # class
 
-  #
-  #
-  #
-  class BaseStack
-    attr_accessor :components
-    #attr_reader :type
-    attr_reader :config
-    attr_accessor :form
-    def initialize config={}, components=[]
-      #@type   = type
-      @config = config
-      config.each do |k, v|
-        instance_variable_set "@#{k}", v
-      end
-      @components = components
-      @calc_needed = true
-    end
-    # XXX if user sets later, we won't be checking the config
-    # We check the actual variables which config sets in init
-    %w[ parent_component width height weight row col orientation].each { |e|
-      eval(
-           "def #{e} 
-              @config[:#{e}]
-            end
-            def #{e}=(val) 
-              @config[:#{e}]=val
-              instance_variable_set \"@#{e}\", val
-              @calc_needed = true
-            end"
-          )
-    }
-    alias :parent :parent_component
-    #alias :parent= :parent_component
-    def repaint # stack
-      $log.debug "XXX: stack repaint recalc #{@calc_needed} "
-      @components.each { |e| e.form = @form unless e.form } #unless @calc_needed
-      recalc if @calc_needed
-      @components.each { |e| e.repaint }
-    end
-    def repaint_all x
-      @calc_needed = true
-    end
-    def override_graphic gr
-      @graphic = gr
-    end
-    def focusable; false; end
-    # Calculates row col and width height
-    # for each subc-omponent based on coords of Container
-    # This is to be called only when the container has got its coordinates (i.e
-    # Containers repaint). This should be in this objects repaint.
-    def recalc
-      @calc_needed = false
-      comp = self
-      if comp.is_a? BaseStack
-        check_coords comp
-        @margin_left   ||= 0
-        @margin_right  ||= 0
-        @margin_top    ||= 0
-        @margin_bottom ||= 0
-        if comp.is_a? Stack
-          r   = row + @margin_top
-          rem = 0
-          ht = height - (@margin_top + @margin_bottom)
-          if @orientation == :bottom 
-            mult = -1
-            comps = @components.reverse
-            r = row + height - @margin_bottom
-    
-          else
-            mult = 1
-            comps = @components
-   
-          end
-          comps.each { |e| 
-            # should only happen if expandable FIXME
-            e.height = 0.01 * e.weight * (ht - (e.margin_top + e.margin_bottom)) 
-            hround = e.height.floor
-            rem += e.height - hround
-            e.height = hround #- (@margin_top + @margin_bottom)
-            # rounding creates a problem, since 0.5 gets rounded up and we can exceed bound
-            # So i floor, and maintain the lost space, and add it back when it exceeds 1
-            # This way the last components gets stretched to meet the end, which is required
-            # when the height of the stack is odd and there's a left-over row
-            if rem >= 1
-              e.height += 1
-              rem = 0
-            end
-            # Item level margins have not been accounted for when calculating weightages, and
-            # should not be used on the weightage axis
-            r += e.margin_top
-            if @orientation == :bottom
-              r += e.height * mult
-              e.row = r 
-            else
-              e.row = r 
-              r += e.height + 0
-            end
-            e.width = width - (@margin_left + @margin_right + e.margin_left + e.margin_right)
-            e.col = col + @margin_left + e.margin_left # ??? XXX
-            $log.debug "XXX: recalc stack #{e.widget.class} r:#{e.row} c:#{e.col} h:#{e.height} = we:#{e.weight} * h:#{height} "
-            #e.col_offset = col_offset # ??? XXX
-            check_coords e
-            e.repaint_all(true)
-            e.recalc if e.is_a? BaseStack
-          }
-        elsif comp.is_a? Flow
-          c = col + @margin_left #+ col_offset
-          rem = 0
-          wd = width - (@margin_left + @margin_right)
-          # right_to_left orientation
-          if @orientation == :right
-            mult = -1
-            comps = @components.reverse
-            c = col + width - @margin_right
-            $log.debug "XXX:  ORIENT1f recalc #{@orientation} "
-          else
-            mult = 1
-            comps = @components
-            $log.debug "XXX:  ORIENT2f recalc #{@orientation} "
-          end
-          comps.each { |e| 
-            e.width = e.weight * wd  * 0.01
-            wround = e.width.floor
-            rem += e.width - wround
-            e.width = wround
-            # see comment in prev block regarding remaininder
-            if rem >= 1
-              e.width += 1
-              rem = 0
-            end
-            e.height = height - (@margin_top + @margin_bottom) #* weight * 0.01
-            #e.height = e.height.round
-            if @orientation == :right
-              c += e.width * mult # mult 1 or -1
-              e.col = c
-            else
-              e.col = c
-              c += e.width * mult # mult 1 or -1
-            end
-            e.row = row + @margin_top
-            check_coords e
-            $log.debug "XXX: recalc flow #{e.widget.class} r:#{e.row} c:#{e.col} h:#{e.height} = we:#{e.weight} * w:#{width} "
-            e.repaint_all(true)   # why not happening when we change row, hieght etc
-            e.recalc if e.is_a? BaseStack
-          }
-        end
-      else
-        alert "in else recalc DOES NOT COME HERE "
-        comp.col    = comp.parent.col
-        comp.row    = comp.parent.row
-        comp.height = comp.parent.height
-        comp.width  = comp.parent.width
-        $log.debug "XXX: recalc else #{comp.class} r #{comp.row} c #{comp.col} . h #{comp} height w #{comp.width} "
-      end
-    end
-    def check_coords e # stack
-      r = e.row
-      c = e.col
-      if r >= row + height
-        $log.warn "XXX: WARN e.class is out of bounds row #{r} "
-        e.visible = false
-      end
-      if c >= col + width
-        $log.warn "XXX: WARN e.class is out of bounds col #{c} "
-        e.visible = false
-      end
-    end
-    def increase c=@current_component
-      p = self #c.parent_component
-      ci = p.components.index(c)
-      ni = ci + 1
-      if p.components[ni].nil?
-        ni = nil
-      end
-      case p
-      when Flow
-        # increase width of current and reduce from neighbor
-        if ni
-          n = p.components[ni]
-          $log.debug "XXX: INC fl current #{ci}, total#{p.components.count}, next #{n} "
-
-          c.width += 1
-          n.width -= 1
-          n.col   += 1
-        end
-
-      when Stack
-        if ni
-          n = p.components[ni]
-          $log.debug "XXX: INC fl current #{ci}, total#{p.components.count}, next #{n} "
-
-          c.height += 1
-          n.height -= 1
-          n.row   += 1
-        end
-        $log.debug "XXX: INC st current #{ci}, total#{p.components.count} "
-      end
-
-    end
-    def decrease c=@current_component
-      p = self #c.parent_component
-      ci = p.components.index(c)
-      ni = ci + 1
-      if p.components[ni].nil?
-        ni = nil
-      end
-      case p
-      when Flow
-        # increase width of current and reduce from neighbor
-        if ni
-          n = p.components[ni]
-          $log.debug "XXX: INC fl current #{ci}, total#{p.components.count}, next #{n} "
-
-          c.width -= 1
-          n.width += 1
-          n.col   -= 1
-        end
-
-      when Stack
-        if ni
-          n = p.components[ni]
-          $log.debug "XXX: INC fl current #{ci}, total#{p.components.count}, next #{n} "
-
-          c.height -= 1
-          n.height += 1
-          n.row   -= 1
-        end
-        $log.debug "XXX: INC st current #{ci}, total#{p.components.count} "
-      end
-
-    end
-    def to_s
-      @components
-    end
-  end
-  # A stack positions objects one below the other
-  class Stack < BaseStack; end
-  # A flow positions objects in a left to right
-  class Flow  < BaseStack; end
-  #
-  # A wrapper over widget mostly because it adds weight and margins
-  #
-  class Item
-    attr_reader :config, :widget
-    attr_reader :margin_top, :margin_left, :margin_bottom, :margin_right
-    def initialize config={}, widget
-      @config = config
-      config.each do |k, v|
-        instance_variable_set "@#{k}", v
-      end
-      @margin_left   ||= 0
-      @margin_right  ||= 0
-      @margin_top    ||= 0
-      @margin_bottom ||= 0
-      @widget = widget
-    end
-    def weight;  @config[:weight]||100; end
-    def weight=(val); @config[:weight]=val; end
-    def repaint; @widget.repaint; end
-    %w[ form parent parent_component width height row col row_offset col_offset focusable].each { |e|
-      eval(
-           "def #{e} 
-              @widget.#{e} 
-            end
-            def #{e}=(val) 
-              @widget.#{e}=val 
-            end"
-          )
-    }
-    def method_missing(sym, *args, &block)
-      @widget.send sym, *args, &block
-    end
-  end
 end # module
 
 if __FILE__ == $PROGRAM_NAME
