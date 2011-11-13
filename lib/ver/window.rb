5098ad070d7120138900608585511e941e3fa36b
Rahul Kumar <rahulbenegal@gmail.com>
2011-11-13 12:48:52 +0530

    window.rb path of Variable

--- lib/ver/window.rb
+++ lib/ver/window.rb
@@ -65,7 +65,7 @@ module VER
       #$error_message_row = $status_message_row = Ncurses.LINES-1
       $error_message_row ||= Ncurses.LINES-1
       $error_message_col ||= 1 # ask (bottomline) uses 0 as default so you can have mismatch. XXX
-      $status_message ||= Variable.new # in case not an App
+      $status_message ||= RubyCurses::Variable.new # in case not an App
       init_vars
 
 
