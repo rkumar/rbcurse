458dc1cc474a0557d84d6b240d0da847701c86cf
Rahul Kumar <rahulbenegal@gmail.com>
2011-11-14 22:10:58 +0530

    appemaillb.rb next to next_component

--- examples/appemaillb.rb
+++ examples/appemaillb.rb
@@ -44,7 +44,7 @@ class ColumnBrowse < Widget
     # what if user gives in wrong order !!
     @gb = @vim.add :grabbar, :SECOND, 0
     @right2 = @vim.add comp, :SECOND, nil
-    @gb.next(@right2)
+    @gb.next_component(@right2)
     _add_component comp
     @right2
   end
