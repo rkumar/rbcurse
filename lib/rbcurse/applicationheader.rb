3b7bfd72644c4762a2e2a021f71b21810e34fd33
Rahul Kumar <rahulbenegal@gmail.com>
2011-11-16 00:22:13 +0530

    applicationheader.rb fixed for 256colorxterm terminal

--- lib/rbcurse/applicationheader.rb
+++ lib/rbcurse/applicationheader.rb
@@ -7,7 +7,12 @@
 #       Author: rkumar http://github.com/rkumar/rbcurse/
 #         Date: 
 #      License: Same as Ruby's License (http://www.ruby-lang.org/LICENSE.txt)
-#  Last update: use ,,L
+#  Last update: 2011-11-16 - 00:07
+#
+#  CHANGES:
+#              For some terminals, like xterm-256color which were not printing spaces
+#              I've changed to code so only text is printed where it has to with no
+#              padding. These terminals remove the padding color.
 # ----------------------------------------------------------------------------- #
 #
 require 'rbcurse/rwidget'
@@ -50,22 +55,37 @@ module RubyCurses
       return unless @repaint_required
  
       #print_header(htext, posy = 0, posx = 0)
-      print_header(@text1 + " %15s " % @text2 + " %20s" % @text_center , posy=0, posx=0)
-      print_top_right(@text_right)
+      att = get_attrib @attr
       len = @window.width
       len = Ncurses.COLS-0 if len == 0
-      att = get_attrib @attr
-      # even this won't work in xterm-256color
-      #@form.window.mvwchgat(0, 0, len, att, $reversecolor , nil)
+      # print a bar across the screen 
+      @window.attron(Ncurses.COLOR_PAIR(@color_pair) | att)
+      @window.mvhline(@row, @col, 1, len)
+      @window.attroff(Ncurses.COLOR_PAIR(@color_pair) | att)
+      #print_header(@text1 + " %15s " % @text2 + " %20s" % @text_center , posy=0, posx=0)
+
+      # Now print the text in the correct positions with no padding, else some terminal
+      # will blacken the text out.
+      print_header("#{@text1}  #{@text2}") # + " %20s" % @text_center , posy=0, posx=0)
+      print_center("#{@text_center}") # + " %20s" % @text_center , posy=0, posx=0)
+      print_top_right(@text_right)
       @repaint_required = false
     end
     def print_header(htext, r = 0, c = 0)
+      #win = @window
+      #len = @window.width
+      #len = Ncurses.COLS-0 if len == 0
+      #
+      #@form.window.printstring r, c, "%-*s" % [len, htext], @color_pair, @attr
+      @form.window.printstring r, c, htext, @color_pair, @attr
+    end
+    def print_center(htext, r = 0, c = 0)
       win = @window
-      len = @window.width
+      len = win.width
       len = Ncurses.COLS-0 if len == 0
       #
-    #$log.debug " def print_header(#{htext}, #{len} "
-      @form.window.printstring r, c, "%-*s" % [len, htext], @color_pair, @attr
+      #@form.window.printstring r, c, "%-*s" % [len, htext], @color_pair, @attr
+      win.printstring r, ((len-htext.length)/2).floor, htext, @color_pair, @attr
     end
     def print_top_right(htext)
     $log.debug " def print_top_right(#{htext})"
